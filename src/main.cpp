2f6196640dd2767f69ec2e5d3bd6680b24b232c9
Egor Kharlamov <egor@kharlamov.co.uk>
2017-03-01 16:32:13 +0000

    Adding requirements

--- src/main.cpp
+++ src/main.cpp
@@ -181,6 +181,69 @@ void mainLoop()
 //////////////////////////////////////////////////////////
 int main(int argc, char *argv[])
 {
+	//FT_Library ft;
+	//if (FT_Init_FreeType(&ft))
+	//	std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+
+	//FT_Face face;
+	//if (FT_New_Face(ft, "assets/fonts/DEARBORN.ttf", 0, &face))
+	//	std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+
+	//FT_Set_Pixel_Sizes(face, 0, 48);
+
+	//if (FT_Load_Char(face, 'X', FT_LOAD_RENDER))
+	//	std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+
+	//struct Character {
+	//	GLuint     TextureID;  // ID handle of the glyph texture
+	//	glm::ivec2 Size;       // Size of glyph
+	//	glm::ivec2 Bearing;    // Offset from baseline to left/top of glyph
+	//	GLuint     Advance;    // Offset to advance to next glyph
+	//};
+
+	//std::map<GLchar, Character> Characters;
+
+	//gl::PixelStorei(gl::UNPACK_ALIGNMENT, 1); // Disable byte-alignment restriction
+
+	//for (GLubyte c = 0; c < 128; c++)
+	//{
+	//	// Load character glyph 
+	//	if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+	//	{
+	//		std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+	//		continue;
+	//	}
+	//	// Generate texture
+	//	GLuint texture;
+	//	gl::GenTextures(1, &texture);
+	//	gl::BindTexture(gl::TEXTURE_2D, texture);
+	//	gl::TexImage2D(
+	//		gl::TEXTURE_2D,
+	//		0,
+	//		gl::RED,
+	//		face->glyph->bitmap.width,
+	//		face->glyph->bitmap.rows,
+	//		0,
+	//		gl::RED,
+	//		gl::UNSIGNED_BYTE,
+	//		face->glyph->bitmap.buffer
+	//	);
+	//	// Set texture options
+	//	gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE);
+	//	gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE);
+	//	gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR);
+	//	gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR);
+	//	// Now store character for later use
+	//	Character character = {
+	//		texture,
+	//		glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+	//		glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+	//		face->glyph->advance.x
+	//	};
+	//	Characters.insert(std::pair<GLchar, Character>(c, character));
+	//}
+
+
 	// Initialises GLFW: If it fails the program exits
 	if (!glfwInit()) exit(EXIT_FAILURE);
 
