39a834f45cc313d6f10aae04a8bb68ae47e9618e
TomM-F <p14155379@my365.dmu.ac.uk>
2017-05-02 13:34:31 +0100

    world changes

--- include/World.h
+++ include/World.h
@@ -15,6 +15,7 @@ class World : public Scene
 	private:
 		GLSLProgram m_worldShader;
 		GLSLProgram m_freeType;
+		GLSLProgram m_imageType;
 		glm::vec3 m_collectableSpeed; // Speed at which a collectable bounces
 		Freetype* m_pHUD;
 	
@@ -26,17 +27,13 @@ class World : public Scene
 		void setLightParamaters(GLSLProgram * pShader, int i);
 		void SetMatices(GLSLProgram * pShader, mat4 model, mat4 view, mat4 projection);
 	
-		Texture *g_pTexture; // Holds texture
-		Camera m_camera; // Camera which user can control
-		GLFWwindow *m_pWindow; // The window
-		sf::Vector2i m_mousePos; // Holds mouse cursor position
 		SceneReader m_sceneReader; // Reads .xml file in order to create world
 		glm::mat4 m_V, m_P;
 	
 	public:
-		World(sf::Vector2i windowSize);
+		World(GLFWwindow *pWindow, sf::Vector2i windowSize);
 		void initScene(Freetype* pOverlay);
-		void setMousePos(GLFWwindow *pWindow, sf::Vector2i mousepos);
+		void setMousePos(sf::Vector2f mousepos);
 		void update(float fTimeElapsed);
 		void render();
 };
--- src/World.cpp
+++ src/World.cpp
@@ -3,28 +3,25 @@
 using std::string;
 using std::ifstream;
 
-World::World(sf::Vector2i windowSize)
-{
-	m_windowSize = windowSize;
-}
+#define MOVE_VELOCITY 50.0f
+#define ROTATE_VELOCITY 0.0025f
 
+World::World(GLFWwindow *pWindow, sf::Vector2i windowSize)
+{
+	m_pWindow = pWindow;
+
+	m_windowSize = windowSize;
+
+	m_camera.setAspectRatio((float)windowSize.x / windowSize.y);
+}
 
 void World::initScene(Freetype* pOverlay)
 {
-
 	m_pHUD = pOverlay; // Get the Heads up display for the scene
 
 	linkShaders();
 	// Stops rendered models from being transparent
 	gl::Enable(gl::DEPTH_TEST);
-
-
-
-	//////////////////////////////////////////////////////
-	/////////// Vertex shader //////////////////////////
-	//////////////////////////////////////////////////////
-
-	
 
 	m_sceneReader = SceneReader("assets/scenes/Scene.xml");
 
@@ -36,17 +33,18 @@ void World::initScene(Freetype* pOverlay)
 		}
 		/*world.ModelList[i].DrawModel(true, true);*/
 	}
+
+//	HUD->LoadHUDImage("assets/textures/Flag_of_Wales.png", 500.f, 500.f, -90, 30.0f);
 }
 
-void World::setMousePos(GLFWwindow *pWindow, sf::Vector2i mousepos)
+void World::setMousePos(sf::Vector2f mousepos)
 {
-	m_pWindow = pWindow;
 	m_mousePos = mousepos;
 }
 
 void World::linkShaders()
 {
-	try 
+	try
 	{
 		// Shader which allows first person camera and textured objects
 		m_worldShader.compileShader("Shaders/shader.vert");
@@ -54,6 +52,21 @@ void World::linkShaders()
 		m_worldShader.link();
 		m_worldShader.validate();
 		m_worldShader.use();
+	}
+	catch (GLSLProgramException & e)
+	{
+		cerr << e.what() << endl;
+		exit(EXIT_FAILURE);
+	}
+  
+	try
+	{
+		// Shader which allows heads up display
+		m_freeType.compileShader("Shaders/freetype.vert");
+		m_freeType.compileShader("Shaders/freetype.frag");
+		m_freeType.link();
+		m_freeType.validate();
+		m_freeType.use();
 	}
 	catch (GLSLProgramException & e) {
 		cerr << e.what() << endl;
@@ -63,16 +76,18 @@ void World::linkShaders()
 	try
 	{
 		// Shader which allows heads up display
-		m_freeType.compileShader("Shaders/freetype.vert");
-		m_freeType.compileShader("Shaders/freetype.frag");
-		m_freeType.link();
-		m_freeType.validate();
+		m_imageType.compileShader("Shaders/image.vert");
+		m_imageType.compileShader("Shaders/image.frag");
+		m_imageType.link();
+		m_imageType.validate();
 	}
-	catch (GLSLProgramException & e) 
+	catch (GLSLProgramException & e)
 	{
 		cerr << e.what() << endl;
 		exit(EXIT_FAILURE);
 	}
+
+
 }
 
 void World::setLightParamaters(GLSLProgram *pShader, int i)
@@ -134,39 +149,30 @@ void World::SetMatices(GLSLProgram * pShader, mat4 model, mat4 view, mat4 projec
 
 void World::update(float fTimeElapsed)
 {
+	// Calculates the mouse movement
+	sf::Vector2f delta(m_mousePos - sf::Vector2f(m_windowSize.x * 0.5f, m_windowSize.y * 0.5f));
 
-	// Creates camera and view using MVP
-
-	 // Allows first person view changing with mouse movement
-	sf::Vector2i windowOrigin(m_windowSize.x * 0.5, m_windowSize.y * 0.5); // Middle of the screen
-
-	float fYAngle = (windowOrigin - m_mousePos).x / 1000.0f;
-	float fZAngle = (windowOrigin - m_mousePos).y / 1000.0f;
-
+	m_camera.rotate(delta.x*ROTATE_VELOCITY, delta.y*ROTATE_VELOCITY);
 	
-
-	m_V = glm::lookAt
-	(
-		m_camera.getPosition(), // Camera position
-		m_camera.getView(), // Looking at
-		m_camera.getUp() // Up
-	);
-
-	for (int i = 0; i < m_sceneReader.m_modelList.size(); i++)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
 	{
-		if (!m_sceneReader.m_modelList.at(i).getCollected()) // Draw all items except collected collectables
-		{
-			setLightParamaters(&m_worldShader, i);
-		}
+		m_camera.move(glm::vec3(0.0f, 0.0f, -MOVE_VELOCITY*fTimeElapsed));
 	}
-
-	m_P = glm::perspective(m_camera.getFOV(), (float)m_windowSize.x/m_windowSize.y, 1.f, 5000.f); // Sets FOV and vision culls
-	//
-	//m_WorldShader.setUniform("mView", m_V);
-	//m_WorldShader.setUniform("mProjection", m_P);
-
 	
-	m_camera.processUserInput(fYAngle, fZAngle); // Send mouse position data to be processed in order to move camera
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+	{
+		m_camera.move(glm::vec3(-MOVE_VELOCITY*fTimeElapsed, 0.0f, 0.0f));
+	}
+	
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+	{
+		m_camera.move(glm::vec3(0.0f, 0.0f, MOVE_VELOCITY*fTimeElapsed));
+	}
+	
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+	{
+		m_camera.move(glm::vec3(MOVE_VELOCITY*fTimeElapsed, 0.0f, 0.0f));
+	}
 
 	// Makes collectables rotate and bounce
 	for (int i = 0; i < m_sceneReader.m_modelList.size(); i++)
@@ -198,18 +204,15 @@ void World::update(float fTimeElapsed)
 			}
 		}
 	}
-
 }
-
 
 void World::render()
 {
 	// Check depth and clear last frame
 	gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
 
-	
 	m_worldShader.use();
-	SetMatices(&m_worldShader, glm::mat4(1.0f), m_V, m_P);
+	setMatrices(&m_worldShader, glm::mat4(1.0f), m_camera.getView(), m_camera.getProjection());
 	for (int i = 0; i < m_sceneReader.m_modelList.size(); i++)
 	{
 		if (!m_sceneReader.m_modelList.at(i).getCollected()) // Draw all items except collected collectables
